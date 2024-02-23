67ef8b4caddcb2b1620d529c505981e6d9cd3a90
Martin Lo <mdangerlo@gmail.com>
2024-02-24 01:30:15 +0800

    game board displays now

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-project inspired by:
-https://www.youtube.com/watch?v=KBpfB1qQx8w&ab_channel=CodingWithRuss
+import pygame
+from pygame.locals import *
 
-#Part 1:
-    #import modules
-    #create blank game window
-    #setup loop w/ exit event handler
-    #create grid and setup grid list
+pygame.init()
 
+screen_width = 300
+screen_height = 300
 
-#Part 2:
-    #create event handler for clicking on cell
-    #check if cell is empty, then place marker
-    #setup function for drawing markers onto the grid
+screen = pygame.display.set_mode((screen_width,screen_height))  # initialize the screen: looking for size, flags, depth, display, vsync
+pygame.display.set_caption("TicTacToe - By Martin, using PyGame")
 
-#Part 3:
-    #create check winner function including checks for winning combinations
-    #create event handler for post-game
-    #reset game
+def draw_grid():
+    background_color = (255,255,200)
+    grid_color = (50,50,50)
+    screen.fill(background_color)
+
+    for line_index in range(1,3):  #can resuse this variable on 2 dimension bc its a square
+        pygame.draw.line(screen, grid_color,(0,line_index*100),(screen_width, line_index*100))  #expecting parameters in terms of surface, surface type, color, start and end, width
+        pygame.draw.line(screen, grid_color, (line_index * 100, 0),(line_index * 100,screen_height))
+
+run = True
+while run:
+    draw_grid()
+
+    #add event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
